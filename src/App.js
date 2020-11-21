470486a0fda805f4a5963a49d1fe6072b6441253
ahmadfaqih-dev <faqih.elekyoben@gmail.com>
2020-11-21 17:34:28 +0700

    Starting setup, pages and routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import User from './user/pages/User';
+import NewPlace from './places/pages/NewPlace';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <Router>
+    <Switch>
+      <Route path="/" exact>
+        <User/>
+      </Route>
+       <Route path="/places/newplace" exact>
+        <NewPlace/>
+      </Route>
+      <Redirect to ="/" />
+    </Switch>
+  </Router>
+    );
+};
 
-export default App;
+export default App; 
